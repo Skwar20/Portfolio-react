2b980ddc999fa0e659d3aef0bbccbe386240625c
Buszek <ak5224208@gmail.com>
2023-02-20 16:44:01 +0100

    finish project

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
 		<div className="w-full h-auto bg-second overflow-hidden" id="About">
 			<div className="w-3/5 bg-first h-10 -skew-x-[45deg] -translate-x-5 "></div>
 
-			<div className="relative w-full md:max-w-[650px] lg:max-w-[850px] xl:max-w-[1100px] mx-auto flex items-center flex-col py-6 px-4">
+			<div className="relative w-full md:max-w-[650px] lg:max-w-[850px] xl:max-w-[1100px] mx-auto flex items-center flex-col py-6 px-5">
 				<div className="w-[120px] h-[120px] rounded-full overflow-hidden bg-third md:w-[150px] md:h-[150px]">
 					<img src="./me.webp" alt="awatar Karlosa" />
 				</div>
@@ -16,12 +16,12 @@ const AboutUs = () => {
 					17 years<span className="text-white m-1">/</span>Karol <span className="text-white m-1">/</span>
 					Freelance
 				</h3>
-				<p className="mt-5 md:text-base xl:text-lg">
+				<p className="mt-5 md:text-base xl:text-lg text-center">
 					Greetings, my name is Karol. My main goal while creating internet websites is correct "functionality", thats
 					why i care about correct SEO, to make the page highly positioned in our browsers and get as much potentional
 					customers as possible, The optimised code which makes the page more efficient.
 				</p>
-				<div className="w-full mt-4 mb-4 text-lg font-medium transition-all flex items-center justify-evenly lg:justify-start md:mt-5">
+				<div className="w-full mt-4 mb-4 text-lg font-medium transition-all flex items-center justify-evenly lg:justify-center md:mt-5">
 					<button
 						onClick={() => setActiveTab('skills')}
 						className={`${
